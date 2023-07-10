b2ee70bd8f994e9b7b8c56407b27a59f715ed15d
SivaKumar <sivakumar.g@improve10x.com>
2023-07-10 17:10:31 +0530

    I created CurrencyConversionActivity

---
+++ app/src/main/java/com/example/currencyconvertion/network/CurrencyConversionActivity.java
@@ -0,0 +1,16 @@
+package com.example.currencyconvertion.network;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.currencyconvertion.R;
+
+public class CurrencyConversionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency_conversion);
+    }
+}
