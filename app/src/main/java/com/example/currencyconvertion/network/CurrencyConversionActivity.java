2d699e8f66eb0c7137bf19fcdca0003c07a14cc0
SivaKumar <sivakumar.g@improve10x.com>
2023-07-10 17:57:04 +0530

    I wrote binding data.

--- app/src/main/java/com/example/currencyconvertion/network/CurrencyConversionActivity.java
+++ app/src/main/java/com/example/currencyconvertion/network/CurrencyConversionActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.currencyconvertion.R;
+import com.example.currencyconvertion.databinding.ActivityCurrencyConversionBinding;
 
 public class CurrencyConversionActivity extends AppCompatActivity {
-
+    private ActivityCurrencyConversionBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_currency_conversion);
+        binding = ActivityCurrencyConversionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
